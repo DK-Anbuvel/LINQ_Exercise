ddeefd37ac3d85eec35d18ff9071b72aae9d64cb
CIPL1759 <CIPL1759@CIPL-ATC00591>
2025-12-29 18:06:25 +0530

    day 2 Questions

--- LINQ.Exercise/Exercises/Exe_3.cs
+++ LINQ.Exercise/Exercises/Exe_3.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LINQ_Exercises.Exercises
 {
@@ -18,6 +19,63 @@ namespace LINQ_Exercises.Exercises
                          where s > 80
                          select s;
             var result1 = list.Where(e=> e > 80).ToList();
+
+            return result.ToList();
+        }
+        /*
+         * 22. Write a program in C# Sharp to find the strings for a specific minimum length.
+         */
+        public int ReturnMinimumLengthOfStringArray(string[] str)
+        {
+            var result = str.OrderByDescending(s => s.Length).Take(1).Select(s=>s.Length)
+                .FirstOrDefault(); // to convert IEnumerable to int
+            var i = str.Min(s => s.Length);
+            var y = str.Max(s => s.Length);
+            var x = str.MaxBy(s => s.Length);
+
+            var result1 = (from s in str
+                          orderby s.Length descending
+                          select s.Length ).First();
+
+            return result;
+        }
+        /*
+         * 23. Write a program in C# Sharp to generate a cartesian product of two sets.
+         */
+        public List<(string, string)> DisplayCartesianProduct(List<char> characterList, List<int> NumberList)
+        {
+            var result = from c in characterList
+                         from n in NumberList
+                         select (Character: c.ToString(), Number: n.ToString());
+            //           select new
+            //             {
+            //                 Charcter = c.ToString(),
+            //                 Number = n.ToString()
+            //             };
+            //($"Charcter: {c}", $"Number: {n}");
+
+            var result1 = characterList.SelectMany(n => NumberList,
+                (characterList, n) => new {
+                    Chacter = characterList,
+                    NumberList = n });
+
+            //(List<(string, string)>)result;
+            return result.ToList();
+        }
+        /*
+         * 24. Write a program in C# Sharp to generate a cartesian product of three sets.
+         */
+        public List<(string,string,string)> DisplayCartesianProduct(List<char> characterList, List<int> NumberList,List<string> colour)
+        {
+            var result = from c in characterList
+                         from n in NumberList
+                         from col in colour
+                         select (Character: c.ToString(), Number: n.ToString(),col.ToString());
+
+           var result1 = characterList.SelectMany(c=> NumberList,(c,n)=> new  {c,n})
+                                      .SelectMany(cn=>colour,(cn,c)=> new {Character =cn.c.ToString(),
+                                           NumberList=cn.n.ToString(),
+                                           Colour = c.ToString()});
 
             return result.ToList();
         }
--- LINQ.Test/Test/Exercises/Exe3_Tests.cs
+++ LINQ.Test/Test/Exercises/Exe3_Tests.cs
@@ -9,11 +9,12 @@ namespace LINQ_Exercise_Test.Test.Exercises
 {
     public class Exe3_Tests
     {
+        Exe_3 sut = new Exe_3();
         [Fact]
         public void DisplayGreaterThen_ReturnsNumbersGreaterThan80()
         {
             // Arrange
-            var sut = new Exe_3();
+            
             var input = new List<int> { 50, 81, 90, 80, 100, 79 };
             var expected = new List<int> { 81, 90, 100 };
             var input2 = new List<int> { 10, 20, 80 };
@@ -28,6 +29,41 @@ namespace LINQ_Exercise_Test.Test.Exercises
             Assert.Empty(result1);
             Assert.Empty(result2);
         }
-
+        [Fact]
+        public void ReturnMinimumLengthOfStringArrayTest()
+        {
+            //Arrange
+            
+            string[] test1 = { "this", "me", "String" };
+            string[] test2 = { "this", "me", "" };
+            string[] test3 = { "My", "Name", "is","Anbuvel" };
+            //Act
+            var result = sut.ReturnMinimumLengthOfStringArray(test1);
+            var result2 = sut.ReturnMinimumLengthOfStringArray(test2);
+            var result3 = sut.ReturnMinimumLengthOfStringArray(test3);
+            //Assert
+            Assert.Equal(6, result);
+            Assert.Equal(4, result2);
+            Assert.Equal(7, result3);
+        }
+        [Fact]
+        public void DisplayCartesianProductTest()
+        {
+            //Arrange
+           List<char> characterList = ['a', 'b', 'c', 'd' ];
+            List<int> numberList = [ 1, 2, 3, 4 ];
+            //Act
+            var result = sut.DisplayCartesianProduct(characterList, numberList);
+        }
+        [Fact]
+        public void DisplayCartesianProductTest2()
+        {
+            //Arrange
+           List<char> characterList = ['a', 'b', 'c', 'd' ];
+            List<int> numberList = [ 1, 2, 3, 4 ];
+            List<string> color = ["green", "orange", "red"];
+            //Act
+            var result = sut.DisplayCartesianProduct(characterList, numberList, color);
+        }
     }
 }
